1994fa41ca16c6cc45b72ddc4fd7821ea631375e
Mike <mzeno184@gmail.com>
2020-11-08 09:34:36 -0500

    Updated Questions api exceptions

--- server/api/Questions.js
+++ server/api/Questions.js
@@ -8,100 +8,167 @@ const questionRouter = Router();
 const getQuestions = async (req, resp) => {
     const dbconn = await getConnection();
     try {
+        console.log('Attemping to fetch questions...')
         const sqlSelect = 'SELECT * FROM questions';
         dbconn.query(sqlSelect, (err, result) => {
             if (err) {
-                console.log(err);
+                console.log('Database Error in getQuestions: ' + err);
+                resp.send('Database error occurred while tyring to fetch questions. Please try again.')
             }
+            console.log('Questions Fetched');
             resp.send(result);
         });
     }
     catch (err) {
-        console.log('dbCreateUser Error:', err);
+        console.log('Exception Caught Error in getQuestions:', err);
+        resp.send('An error occured while trying to fetch questions:', err)
     }
     finally {
         if (dbconn) {
             dbconn.end();
-            console.log('Connection closed')
         }
     };
 };
 
-// find a blog post
+// find question by id
 const findQuestion = async (req, resp) => {
     const dbconn = await getConnection();
-    const id = req.params.id
-    const sqlSelect = 'SELECT * FROM posts WHERE id = ?';
-    dbconn.query(sqlSelect, id, (err, result) => {
-        if (err) {
-            console.log(err);
+    try {
+        const id = req.params.id
+        console.log(`Attemping to fetch Question id:${id}...`)
+        const sqlSelect = 'SELECT * FROM questions WHERE id = ?';
+        dbconn.query(sqlSelect, id, (err, result) => {
+            if (err) {
+                console.log('Database Error in findQuestion: ' + err);
+                resp.send(`Database error occurred while trying to fetch Question id:${id}. Please try again.`)
+            }
+            console.log(`Question id:${id} Found`);
+            resp.send(result);
+        });
+    }
+    catch (err) {
+        console.log('Exception Caught Error in findQuestion:', err);
+        resp.send(`An error occured while trying to fetch Question id:${id}:`, err)
+    }
+    finally {
+        if (dbconn) {
+            dbconn.end();
         }
-        resp.send(result);
-    });
+    };
 };
 
 // create new question
 const addQuestion = async (req, resp) => {
     const dbconn = await getConnection();
     try {
+        console.log('Attemping to create question...')
         const title = req.body.title;
         const body = req.body.body;
         const userid = req.body.userid
-        const categoryid = 1
+        const categoryid = 1  //hard coded needs logic
         console.log('title:', title, 'body:', body, 'userid:', userid, 'categoryid', categoryid)
         const sqlInsert = 'INSERT INTO questions (title, body, userid, categoryid) VALUES (?, ?, ?, ?)';
         dbconn.query(sqlInsert, [title, body, userid, categoryid], (err, result) => {
             if (err) {
-                console.log(err);
+                console.log('Database Error in addQuestion: ' + err);
+                resp.send('Database error occurred while trying to add Question. Please try again.')
             }
             console.log('Question created:', result);
+            resp.send('Question created:', result);
         });
     }
     catch (err) {
-        console.log('addQuestion Error:', err);
+        console.log('Exception Caught Error in addQuestion:', err);
+        resp.send('An error occured while trying to Ask Question:', err)
     }
     finally {
         if (dbconn) {
             dbconn.end();
-            console.log('Connection closed')
         }
     };
 };
 
-// delete a blog post
+// delete question
 const deleteQuestion = async (req, resp) => {
     const dbconn = await getConnection();
-    const id = req.params.id
-    const sqlDelete = 'DELETE FROM posts WHERE id = ?';
-    dbconn.query(sqlDelete, id, (err, result) => {
-        if (err) console.log(err)
-    });
+    try {
+        const id = req.params.id
+        console.log(`Attemping to delete Question id:${id}...`)
+        const sqlDelete = 'DELETE FROM questions WHERE id = ?';
+        dbconn.query(sqlDelete, id, (err, result) => {
+            if (err) {
+                console.log('Database Error in deleteQuestion: ' + err);
+                resp.send('Database error occurred while deleting Question. Please try again.')
+            }
+            console.log('Question Deleted:', result);
+            resp.send(`Question id:${id} Deleted`);
+        });
+    }
+    catch (err) {
+        console.log('Exception Caught Error in deleteQuestion:', err);
+        resp.send(`An error occured while trying to delete Question id:${id}:`, err)
+    }
+    finally {
+        if (dbconn) {
+            dbconn.end();
+        }
+    };
 };
 
 
-// update a blog post
+// update question
 const updateQuestion = async (req, resp) => {
     const dbconn = await getConnection();
-    console.log('trying to update blog post...')
-    const id = req.body.id;
-    const postText = req.body.postText
-    const sqlUpdate = 'UPDATE posts SET post_text = ? WHERE id = ?';
-    dbconn.query(sqlUpdate, [postText, id], (err, result) => {
-        if (err) console.log(err)
-    });
+    try {
+        const id = req.body.id;
+        const body = req.body.postText
+        console.log(`Attemping to delete update Question id:${id}...`)
+        const sqlUpdate = 'UPDATE questions SET body = ? WHERE id = ?';
+        dbconn.query(sqlUpdate, [body, id], (err, result) => {
+            if (err) {
+                console.log('Database Error in updateQuestion: ' + err);
+                resp.send('Database error occurred while updating Question. Please try again.')
+            }
+            console.log('Question updated:', result);
+            resp.send(`Question id:${id} updated`);
+        });
+    }
+    catch (err) {
+        console.log('Exception Caught Error in updateQuestion:', err);
+        resp.send(`An error occured while trying to update Question id:${id}:`, err)
+    }
+    finally {
+        if (dbconn) {
+            dbconn.end();
+        }
+    };
 };
 
-// like a blog post
+// vote question
 const voteQuestion = async (req, resp) => {
     const dbconn = await getConnection();
-    const id = req.params.id;
-    const sqlUpdate = 'UPDATE posts SET likes = likes + 1 WHERE id = ?';
-    dbconn.query(sqlUpdate, id, (err, result) => {
-        if (err) {
-            console.log(err)
+    try {
+        const id = req.params.id;
+        console.log(`Attemping to vote for Question id:${id}...`)
+        const sqlUpdate = 'UPDATE questions SET votes = votes + 1 WHERE id = ?';
+        dbconn.query(sqlUpdate, id, (err, result) => {
+            if (err) {
+                console.log('Database Error in voteQuestion: ' + err);
+                resp.send('Database error occurred while applying vote. Please try again.')
+            }
+            console.log(result)
+            resp.send(`Question id:${id} vote applied.`);
+        });
+    }
+    catch (err) {
+        console.log('addQuestion Error:', err);
+        resp.send(`An error occured while trying to vote for Question id:${id}:`, err)
+    }
+    finally {
+        if (dbconn) {
+            dbconn.end();
         }
-        console.log(result)
-    });
+    };
 };
 
 // blog routes
