ec0ded08275756226eb5fab18deba3bfda1976bf
Mike <mzeno184@gmail.com>
2020-11-11 16:00:33 -0500

    Created Ask button

---
+++ client/src/pages/AskButton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const AskButton = (
+        <>
+           <li id="ask-li" className="navrow"><button id="askButton" type="button" class="btn btn-primary btn-lg btn-block">Ask</button></li>
+        </>
+    )
+
